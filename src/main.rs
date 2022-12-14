e49af67b7f5b57f91e986ef15e51102713e6f701
Mario Olcina <marioolcinaconejos@gmail.com>
2022-12-14 13:59:36 +0100

    Working submit. Well kinda

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+use rocket::request::Form;
+use rocket::http::RawStr;
+use rocket::response::content::Html;
+
+#[derive(FromForm)]
+struct UserInput<'f> {
+    // The raw, undecoded value. You _probably_ want `String` instead.
+    value: &'f RawStr
+}
+
+#[post("/submit", data = "<user_input>")]
+fn submit_task(user_input: Form<UserInput>) -> String {
+    format!("Your value: {}", user_input.value)
+}
+
+#[get("/")]
+fn hello() -> rocket::response::content::Html<&'static str> {
+    Html(include_str!("main.html"))
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+    .mount("/", routes![hello])
+    .mount("/", routes![submit_task])
+    .launch();
 }
